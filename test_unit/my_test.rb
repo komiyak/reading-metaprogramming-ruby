d92eb33e6e9c453675a1df9d82c42e01da91b0bf
Kosuke Komiya <kosuke.komiya@gmail.com>
2019-05-05 15:58:06 +0900

    Add a scuseeded test.

---
+++ test_unit/my_test.rb
@@ -0,0 +1,13 @@
+require 'test/unit'
+
+class MyTest < Test::Unit::TestCase
+  # def setup
+  # end
+
+  # def teardown
+  # end
+
+  def test_fail
+    assert(true, 'Assertion was false.')
+  end
+end
