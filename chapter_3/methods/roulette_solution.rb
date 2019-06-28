ae2fff37b23955bc51d492862f208b19fd4fe7f0
Kosuke Komiya <kosuke.komiya@gmail.com>
2019-06-28 14:40:42 +0900

    Add a limitation can use specific user name.

---
+++ chapter_3/methods/roulette_solution.rb
@@ -0,0 +1,19 @@
+class Roulette
+  def method_missing(name, *args)
+    person = name.to_s.capitalize
+
+    super unless %w[Bob Frank Bill].include? person
+    
+    3.times do
+      number = rand(10) + 1
+      puts "#{number}..."
+    end
+
+    "#{person} got a #{number}"
+  end
+end
+
+# Using
+number_of = Roulette.new
+puts number_of.bob
+puts number_of.frank
