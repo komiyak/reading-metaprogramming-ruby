2f35255f6ac2741f8483c24ef559dcd0e2d8a650
Kosuke Komiya <kosuke.komiya@gmail.com>
2019-08-23 17:28:50 +0900

    Add a_singleton_method to the obj.

--- chapter_5/rat.rb
+++ chapter_5/rat.rb
@@ -8,3 +8,14 @@ class D < C; end
 
 obj = D.new
 p obj.a_method
+
+class << obj
+  def a_singleton_method
+    'obj#a_singleton_method()'
+  end
+end
+
+#p obj.a_singleton_method
+
+p obj.singleton_class
+p obj.singleton_class.superclass
